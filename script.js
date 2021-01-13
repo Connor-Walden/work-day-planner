f47172d4d70893e2bc7b4bc20833b0a25847e176
Connor Walden <connorwalden1403@gmail.com>
2021-01-13 13:45:01 +1030

    implemented hour column (displays the current hour on a 2 wide col for every row)

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var NUM_HOURS = 8;
+const NUM_HOURS = 8;
+const START_HOUR = 9;
 
 $(document).ready(function() {
 
   // Time block creation
 
-  for(var i = 0; i < NUM_HOURS; i++) {
+  for(var i = 0; i < NUM_HOURS + 1; i++) {
     // The row that contains all of the information for this hour of the day planner
     var timeBlockEl = $("<div>");
     timeBlockEl.attr("class", "row");
@@ -13,7 +14,13 @@ $(document).ready(function() {
     // Now, append a bootstrap column for the left side of the row (time slot)
     var timeSlotEl = $("<div>");
     timeSlotEl.attr("class", "col-2");
+    timeSlotEl.attr("style", "border-top: 4px solid black;");
 
+    var hour = (i + START_HOUR) % 12;
+
+    timeSlotEl.append("<h2>" + (hour == 0 ? 12 : hour) + "</h2>");
+
+    timeBlockEl.append(timeSlotEl);
     $(".container").append(timeBlockEl);
   }
 })
